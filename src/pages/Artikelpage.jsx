8184bbc555c82af7531f499c614185eb57ce0fa3
khaliladha11 <khaliladha1123@gmail.com>
2024-06-05 15:17:42 +0700

    konfigurasi artikel namun masih gagal

--- src/pages/Artikelpage.jsx
+++ src/pages/Artikelpage.jsx
@@ -1,11 +1,57 @@
-import React from 'react'
-import { Container, Col, Row, Card } from 'react-bootstrap'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
-function Artikelpage() {
-    return (
-        <div>Artikelpage</div>
-    )
+function ArtikelPage() {
+  // ... (rest of your code)
+
+return (
+        <div>
+        <header style={{ width: "100%", height: "620px", backgroundColor: "#E38B54" }}>
+            <div style={{ width: "100%", maxWidth: "1200px", margin: "0 auto" }}>
+            <Row>
+                <Col>
+                <div className="header-content" style={{ paddingTop: "110.5px" }}> {/* Added style here */}
+                    <h3>
+                    <span style={{ 
+                        fontSize: "42px", 
+                        fontStyle: "normal",
+                        fontWeight: 600,
+                        lineHeight: "normal",
+                        marginBottom: "30px",
+                        color:"#ffffff"
+                    }}>
+                        Cara merawat anjing agar tetap sehat
+                    </span>
+                    </h3>
+                    <p style={{ 
+                        fontSize: "28px", 
+                        fontStyle: "normal", 
+                        fontWeight: 400,
+                        marginBottom: "50px",
+                        color:"#ffffff"
+                    }}>
+                    Berikan Makan yang Bernutrisi. Cara merawat anak anjing hingga
+                    dewasa adalah dengan memberikan makanan yang tepat. Tujuannya
+                    agar perkembangan hewan dapat sesuai dengan usianya.
+                    </p>
+                    <button style={{ 
+                        borderRadius: "15px",
+                        background: "#D05440",
+                        color: "white",
+                        padding: "10px 20px",
+                        border: "none",
+                    }}>Baca selengkapnya</button>
+                </div>
+                </Col>
+                <Col>
+                <img src="../public/komunitasHomepage.png" alt="Hero-img" className="header-image" />
+                </Col>
+            </Row>
+            </div>
+        </header>
+
+        </div>
+    );
 }
 
-export default Artikelpage
+export default ArtikelPage;
